72f16641a2ad2d9aaba031f8602037cc5c5547d1
trilok9999 <s521950@mail.nwmissouri.edu>
2016-04-21 08:52:39 -0400

    Constructor

--- issues.rb
+++ issues.rb
@@ -11,23 +11,26 @@ module Planner
 
   #retrieving Pre-Requisites for the given Issue
   def get_todo_list
-      "Here are the things you have to do before hand #{self.prerequisities.join(" ")}"
+      "Here are the things you have to do before hand #{self.prerequisities}"
   end
 
   #Filtering Issues based on Percentage Completed
   def filter_list list
     list.reject! { |i| i.percentComplete.to_i < 90 }
+    list
   end
 end
 class Issue
   include Comparable,Planner
-attr_accessor :name,:priority,:estimatedManHours,:percentComplete,:prerequisities #Getters and Setters
+attr_accessor :name,:priority,:estimatedManhours,:percentComplete,:prerequisities #Getters and Setters
   def initialize issue
     @name,@priority,@estimatedManhours,@percentComplete,*@prerequisities=issue #parallel assignment
     @percentComplete=@percentComplete.to_f
   end
+
+  #over riding to_string method
   def to_s
-    "#{name} This task has a priority of #{priority} and estimated man hours for this are #{estimatedManHours}. Percentage of work finished up to now is #{percentComplete}
+    "#{name} : This task has a priority of #{priority} and estimated man hours for this are #{estimatedManhours}. Percentage of work finished up to now is #{percentComplete}
 and PreRequisities for this task are #{prerequisities.join(" ")}"
   end
   def <=> (other)
@@ -49,6 +52,10 @@ File.open("ruby.txt", "r+") do |f|
     hash[arr[0]]=Issue_Array.last
   end
 end
+
 p Issue_Array
 puts "The TODO list for the given task is #{Issue_Array[1].get_todo_list}"
-puts "Filtering List Based on Percent Complete #{Issue.filter_list Issue_Array}"
+arr=Issue.filter_list Issue_Array
+arr.each do |i|
+  puts i
+end
